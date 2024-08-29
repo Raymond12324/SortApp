0797644bfc3bfadc11f146fc47326f592dea9981
Randolph Raymond Carmona <raymondcarmonatejeda01@gmail.com>
2024-08-28 20:26:03 -0400

    chore: Refactor LinkedList class and add methods for length, accessing elements, swapping, comparing, and printing

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -1,7 +1,46 @@
-class Node {
-    next: Node | null = null; 
+class ElementNode {
+  next: ElementNode | null = null;
 
-  constructor(public data: number) {
-    
+  constructor(public data: number) {}
+}
+
+class LinkedList {
+  head: ElementNode | null = null;
+
+  add(value: number): void {
+    const node = new ElementNode(value);
+
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let tail = this.head;
+    while (tail.next) {
+      tail = tail.next;
+    }
+
+    tail.next = node;
   }
+
+  get length(): number {
+    if (!this.head) {
+      return 0;
+    }
+    return 0;
+  }
+
+  at(index: number): void {
+    if (!this.head) {
+      throw new Error("Index out of bounds");
+    }
+  }
+
+  swap(leftHand: number, rightHand: number): void {}
+
+  compare(leftHand: number, rightHand: number): boolean {
+    return true;
+  }
+
+  print(): void {}
 }
